4b61bb71f0b7e5c18f25eb1f15c8b0a59d8fd909
Jewel-Thomas <jewelthomas20@gmail.com>
2026-08-05 13:11:36 +0530

    commiting changes to rename branch

--- Pulse/src/Pulse/Renderer/ShaderSrc.h
+++ Pulse/src/Pulse/Renderer/ShaderSrc.h
@@ -28,7 +28,6 @@ namespace Pulse::ShaderSrc {
 				#version 330 core
 				layout(location = 0) out vec4 color;
 
-				in vec3 v_Position;
 				in vec4 v_Color;
 
 				void main()
--- Sandbox/src/RenderLayer.cpp
+++ Sandbox/src/RenderLayer.cpp
@@ -36,12 +36,14 @@ void RenderLayer::OnAttach()
 	m_VertexArray->AddVertexBuffer(vertexBuffer);
 
 	// Index Buffer
-	unsigned int indices[36] = { 0, 1, 2, 2, 3, 0, 
-								 4, 5, 6, 6, 7, 4, 
-								 6, 2, 3, 3, 7, 6, 
-								 1, 5, 4, 4, 0, 1, 
-								 1, 5, 6, 6, 2, 1, 
-								 0, 3, 7, 7, 4, 0 };
+	unsigned int indices[36] = { 
+		0, 1, 2, 2, 3, 0, 
+		4, 5, 6, 6, 7, 4, 
+		6, 2, 3, 3, 7, 6, 
+		1, 5, 4, 4, 0, 1, 
+		1, 5, 6, 6, 2, 1, 
+		0, 3, 7, 7, 4, 0 
+	};
 
 	std::shared_ptr<Pulse::IndexBuffer> indexBuffer;
 	indexBuffer.reset(Pulse::IndexBuffer::Create(indices, sizeof(indices) / sizeof(indices[0])));
@@ -58,6 +60,8 @@ void RenderLayer::OnUpdate()
 	Pulse::RenderCommand::Clear();
 
 	Pulse::Renderer::BeginScene();
+
+	// Camera stuff returns may the view matrix
 
 	m_Shader->Bind();
 	Pulse::Renderer::Submit(m_VertexArray);
